a2678648d144f0154d5dfec71531f6a30d66764b
Xeniya Budyugina <147890566+Xeni-ya@users.noreply.github.com>
2025-03-12 20:55:43 +0300

    init: create divide.js

---
+++ divide.js
@@ -0,0 +1,13 @@
+// function divide(a, b) {
+//   retern a / b;
+// }
+
+// console.log(divide(10, 2))
+
+// function divide(a, b) {
+//   retern a / b;
+// }
+
+// module.exports = {
+//   divide: divide;
+// }
